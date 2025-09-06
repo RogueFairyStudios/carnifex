ba84489609d45cb84006d187125515e55c7685c3
Edward 'Toy' Facundo <edward@fromabyss.com>
2025-09-06 18:39:45 +0100

    Complete IronWail to Carnifex Engine renaming
    - Updated all remaining IronWail references to Carnifex
    - Changed initialization message to use CARNIFEX_VER_STRING
    - Updated version display in host.c to show Carnifex
    - Changed engine string in CSQC_Init to 'Carnifex'
    - Updated shader error messages to show Carnifex engine
    - Removed IronWail reference from console title
    - Updated Windows Makefiles to generate carnifex-engine.exe
    - Updated config file references to carnifex.cfg
    
    All IronWail references have been completely replaced with Carnifex branding while preserving original credits.

--- Quake/common.c
+++ Quake/common.c
@@ -2815,7 +2815,7 @@ static void COM_AddBaseDir (const char *path)
 COM_MigrateNightdiveUserFiles
 
 Checks the Nightdive dir for subdirs containing
-an ironwail.cfg file and moves known files over
+a carnifex.cfg file and moves known files over
 to the new dir
 =================
 */
